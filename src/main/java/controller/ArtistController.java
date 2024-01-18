2bcbe52b6fb0f4f5912ece26e67e3d687946a96b
heizeris <ktomask464@gmail.com>
2024-01-18 12:59:12 +0200

    First commit

---
+++ src/main/java/controller/ArtistController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import model.response.ArtistResponse;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@Slf4j
+@Validated
+@RestController
+@RequestMapping("/artist")
+@RequiredArgsConstructor
+public class ArtistController {
+
+
+    private final ArtistController artistService;
+
+
+    @GetMapping
+    public ArtistResponse getSongsByArtist(
+            @RequestParam @NotBlank String artist
+
+    ) throws IOException, InterruptedException{
+        log.debug("Called get Artist works endpoint with Artist: {}", artist);
+
+        return artistService.getSongsByArtist(artist);
+    }
+
+}
