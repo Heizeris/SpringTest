6c704b8245eb815bb909096f4c186636a2f52a8a
heizeris <ktomask464@gmail.com>
2024-01-18 12:40:15 +0200

    First commit

---
+++ src/main/java/model/response/Artist.java
@@ -0,0 +1,16 @@
+package model.response;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class Artist {
+
+    private String artist;
+
+    private String songTitle;
+
+}
